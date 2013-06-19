aceeef99264ec1cd230b22796798074ac00ea7ac
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-06-19 15:27:48 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestEy::Application.config.secret_token = '8a1e51ffab9ae479affaf74b7d5ce53a7b8f335e68ad9b5bc4aac2b5dc67dd8a84b0e143ee36060fb2e406c92729e986f4af1e69aef5665b69791e5d91ed2f84'
